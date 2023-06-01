c8e12addd491036cfd8b7976ba8c48b506de0b44
unknown <timmortal44@yahoo.com>
2023-06-01 23:46:48 +0100

    init res for await data from api

--- app/page.js
+++ app/page.js
@@ -4,5 +4,9 @@ export default async function Home() {
 
   const data = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`)
 
- 
+  const res = await data.json()
+  // console.log(res)
+  // console.log('API is live')
+
+  
 }
