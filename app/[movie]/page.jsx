c5c5350028d4125608ea07b3084e96542a469dd0
unknown <timmortal44@yahoo.com>
2023-06-29 22:17:23 +0100

    export async fnc

--- app/[movie]/page.jsx
+++ app/[movie]/page.jsx
@@ -1,5 +1,9 @@
 import Image from "next/image";
 
+// export const revalidate = 12; //says, he kind of found this buggy and didn't work all of the time
+
+// we want to pre-render the pages, serve them static
+export  async function generateStaticParams() {
 }
 
 
