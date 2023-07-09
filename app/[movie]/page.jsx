8becf651dc8f92f4c34e19a1de9fede2be96525f
unknown <timmortal44@yahoo.com>
2023-07-09 23:43:01 +0100

    include img path

--- app/[movie]/page.jsx
+++ app/[movie]/page.jsx
@@ -43,6 +43,7 @@ export default async function MovieDetail({ params }) {
         <h2 className="bg-green-600 text-sm inline-block my-2 py-2 px-4 rounded-md">{res.status}</h2>
         <Image 
             className="my-12 w-full" 
+            src={imagePath + res.backdrop_path} 
         />
         <p></p> 
         </div>
